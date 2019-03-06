a8e777f90a3b7f50cec41341068f981a259b6581
Mag Faris <mfaris627@gmail.com>
2019-03-06 16:57:27 -0500

    Change the message displayed by cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment2
+## The makeCacheMatrix and cacheSolve functions below cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## The function creates a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+             m <<- NULL
+	     set <- function(y){
+                   x <<- y
+                   m <<- NULL
+	     }
+            get <- function() x
+            setInverse <- function(inverse) m <<- inverse
+            getInverse <- function()m
+            list(set = set, get = get,
+	    	setInverse = setInverse,
+		getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix returned by makeCacheMartix function. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$get()
+        if (!is.null(m) {
+               message("getting cached data")
+	       return(m)
+	}
+	data <- x$getInverse()
+	m <- solve(data, ...)
+        x$setInverse(m)
+	m
 }
